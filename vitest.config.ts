7dbd94fd33b12c4c843b25ad5d98ad228ecfdcc9
diyanqi <60802943+diyanqi@users.noreply.github.com>
2026-04-13 14:53:46 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ vitest.config.ts
@@ -0,0 +1,15 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, '.'),
+    },
+  },
+  test: {
+    include: ['tests/**/*.test.ts'],
+    exclude: ['tests/**/*.eval.test.ts'],
+    setupFiles: ['tests/setup-env.ts'],
+  },
+});
